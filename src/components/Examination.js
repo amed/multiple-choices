47b66289042a466499ecc83b32c56ef849457e3e
Amed Kino <amed@xbyte.team>
2017-12-14 23:49:00 +0100

    Building basic structure of Examination component

--- src/components/Examination.js
+++ src/components/Examination.js
@@ -5,10 +5,37 @@ import {selectChoice} from '../redux/actions';
 import './Examination.css';
 
 class Examination extends Component {
+  state = { currentQuestions: null }
+
+  startQuiz () {
+    this.selectQuestion(1)
+  }
+
+  selectQuestion (questionId) {
+    this.setState({currentQuestions: questionId})
+  }
+
   render() {
+    const {currentQuestions} = this.state
     return (
-      <div className="Examination container">
-        <h1>Examination</h1>
+      <div className="Examination container m-5">
+        <h1 className="p-3">Examination</h1>
+        {(currentQuestions === null)
+          ? (
+            <div>
+              <p className="py-3">If you are ready please click start</p>
+              <p className="py-3 text-center">
+                <a href="#" className="btn btn-lg btn-primary" onClick={ () => {this.startQuiz()} }>
+                  START
+                </a>
+              </p>
+            </div>
+          )
+        : (
+            <p className="py-3">You clicked start</p>
+          )
+      }
+
       </div>
     );
   }
